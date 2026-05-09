ddda3e73167a89af7bc68cb0b5409e6646e115ca
Umesh Hakke <umeshhakke050@gmail.com>
2026-05-09 23:02:22 +0530

    backend api updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://projecthub-397q.onrender.com/api',
 });
 
 // ✅ Add token only once
